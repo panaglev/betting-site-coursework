4b9cbb33df67db3f479835c10ac2c876c8076031
Ivan Panaev <panaev2395@gmail.com>
2023-01-15 20:28:47 +0700

    feat(): sign-in and sign-up page handlers added; fix smOl things-s-s-s

--- app.py
+++ app.py
@@ -12,22 +12,73 @@ class BetsView(Resource):
     def get(self):
         with sqlite3.connect("coursework.db") as connection:
             cursor = connection.cursor()
-            cursor.execute("SELECT * FROM users")
-            return cursor.fetchall()
+            cursor.execute("SELECT * FROM bets")
+            return cursor.fetchall(), 200 # 200 - OK
             
-    def post(self):
+    def post(self): # change to create bet 
         req_json = request.get_json()
-        print(req_json) # {'login': 'my_login', 'password': 'my_super_password'}
         with sqlite3.connect("coursework.db") as connection:
             cursor = connection.cursor()
             login = req_json["login"]
             password = req_json["password"]
             password = sha256((password+HASH_SALT).encode('utf-8')).hexdigest()
-            #cursor.execute(f"INSERT INTO users (login, pass_hash, balance) VALUES ('{login}', '{password}', 0)") <- not safe to do
-            #cur.execute("SELECT * FROM userdata WHERE Name = %s;", (name,)) <- safe to do 
             cursor.execute("INSERT INTO users (login, pass_hash, balance) VALUES ('%s', '%s', 0);"%(login, password,))
             connection.commit()
-        return "", 200
+        return "", 201 # 201 - Created
+
+@api.route("/sign-in")
+class SignInView(Resource):
+    def get(self):
+        with sqlite3.connect("coursework.db") as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM users") # Display login page
+            return cursor.fetchall()
+
+    def post(self):
+        req_json = request.get_json()
+        with sqlite3.connect("coursework.db") as connection:
+            cursor = connection.cursor()
+            login = req_json["login"]
+            password = req_json["password"]
+            password = sha256((password+HASH_SALT).encode('utf-8')).hexdigest()
+            cursor.execute("SELECT * FROM users WHERE login = '%s' and pass_hash = '%s';"%(login, password,))
+            if cursor.fetchall() != []:
+                return "", 200 # 200 - OK
+            else:
+                return "No such user or password is incorrect", 401 # 401 - Unauthorized
+            
+
+@api.route("/sign-up")
+class SignUpView(Resource): # Display login page or what's for this required
+    def get(self):
+        with sqlite3.connect("coursework.db") as connection:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM users") 
+            return cursor.fetchall()
+
+    def post(self):
+        req_json = request.get_json()
+        with sqlite3.connect("coursework.db") as connection:
+            cursor = connection.cursor()
+
+            # Parse data from json 
+            login = req_json["login"]
+            password = req_json["password"]
+            password = sha256((password+HASH_SALT).encode('utf-8')).hexdigest()
+            
+            # Check if user already in db to avoid Broken Access controll
+            # Top 1 of vulns by OWASP Top 10 
+            cursor.execute("SELECT * FROM users WHERE login = '%s';"%(login,))
+            if cursor.fetchall() == []:
+                # Using params, not f-strings to input data in sql query
+                # Avoid SQL Injection, Top 3 vuln by OWASP Top 10
+                #cursor.execute(f"INSERT INTO users (login, pass_hash, balance) VALUES ('{login}', '{password}', 0)") <- not safe to do
+                #cur.execute("SELECT * FROM userdata WHERE Name = %s;", (name,)) <- safe to do 
+                cursor.execute("INSERT INTO users (login, pass_hash, balance) VALUES ('%s', '%s', 0);"%(login, password,))
+                connection.commit()
+                return "", 201 # 201 - Created
+            else:
+                return "This user already exist", 409 # 409 - Conflict
 
 if __name__ == "__main__":
     app.run(debug=True)
