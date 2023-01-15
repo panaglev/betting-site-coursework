045eef54bde626c32323ea72f961a381ab7a0672
Ivan Panaev <panaev2395@gmail.com>
2023-01-15 15:49:44 +0700

    feat(): pass hashing w/ salt

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import sqlite3
 from flask import Flask, request
 from flask_restx import Api, Resource
+from hashlib import sha256
 
 app = Flask(__name__)
 api = Api(app)
+HASH_SALT = "9fc47da85894433819877a9d0e3f01f6ff35afeb25cc6058d138284abd3a050b"
 
 @api.route("/bets")
 class BetsView(Resource):
@@ -20,6 +22,7 @@ class BetsView(Resource):
             cursor = connection.cursor()
             login = req_json["login"]
             password = req_json["password"]
+            password = sha256((password+HASH_SALT).encode('utf-8')).hexdigest()
             #cursor.execute(f"INSERT INTO users (login, pass_hash, balance) VALUES ('{login}', '{password}', 0)")
             #cur.execute("SELECT * FROM userdata WHERE Name = %s;", (name,)) <- safe to do 
             cursor.execute("INSERT INTO users (login, pass_hash, balance) VALUES ('%s', '%s', 0);"%(login, password,))
